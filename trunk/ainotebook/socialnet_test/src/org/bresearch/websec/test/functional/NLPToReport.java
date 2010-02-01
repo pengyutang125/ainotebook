d3252babb8b391d7438e54bd5413671be2b0b1e5
berlin.brown <berlin.brown@45ad1f16-f7af-11de-a831-516a40b15b2e>
2010-02-01 06:15:08 +0000

    Adding nlp testing

---
+++ trunk/ainotebook/socialnet_test/src/org/bresearch/websec/test/functional/NLPToReport.java
@@ -0,0 +1,74 @@
+package org.bresearch.websec.test.functional;
+
+import java.io.File;
+import java.util.List;
+
+import org.bresearch.websec.parse.IParseDocument;
+import org.bresearch.websec.parse.ParseDocument;
+import org.bresearch.websec.parse.ParseDocumentModule;
+import org.bresearch.websec.parse.model.Sentence;
+import org.bresearch.websec.utils.FileUtil;
+import org.bresearch.websec.utils.botlist.report.IReport;
+import org.bresearch.websec.utils.botlist.report.ReportDocument;
+import org.bresearch.websec.utils.botlist.report.ReportModule;
+import org.bresearch.websec.utils.botlist.report.ReportTermsDocument;
+import org.bresearch.websec.utils.botlist.report.ReportTermsModule;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
+public class NLPToReport {
+   
+    public static List<Sentence> getSentences() throws Exception {        
+        
+        FileUtil util = new FileUtil();
+        String data = util.readLinesRaw(new File("./misc/oz_small.txt"));        
+        
+        final String modelPath = "../socialnet/models/tag.bin.gz";
+        final String dictFile = "../socialnet/models/tagdict";
+        
+        final Injector injector = Guice.createInjector(new ParseDocumentModule(data, modelPath, dictFile));
+        final IParseDocument reportTerms = injector.getInstance(ParseDocument.class);
+        return reportTerms.parse();
+        
+    }
+    
+    public static String buildDocument(final List<Sentence> sentences) {
+        
+        StringBuilder builder = new StringBuilder(100);
+        for (Sentence sent : sentences) {
+            
+            final boolean hasOneTerm = sent.hasOnlyOneTerm(sent.toDistinctFullWordForm());
+            if (!hasOneTerm) {
+                builder.append(sent.toDistinctFullWordForm());
+                builder.append(' ');
+            }
+        } // End of the for //
+        return builder.toString();
+    }
+    
+    
+    public static void report(String doc) {
+        String data = doc;        
+        final Injector injector = Guice.createInjector(new ReportModule(data, true));             
+        final IReport report = injector.getInstance(ReportDocument.class);
+        
+        final Injector injectorTerms = Guice.createInjector(new ReportTermsModule(data));
+        final ReportTermsDocument reportTerms = injectorTerms.getInstance(ReportTermsDocument.class);        
+        reportTerms.setNumTopWords(2000);        
+        
+        System.out.println();
+        System.out.println(report.toReport());        
+        //System.out.println(reportTerms.toReport());
+        System.out.println("Done");
+
+    }
+    
+    public static void main(String [] args) throws Exception {
+        
+        System.out.println("Loading file");
+        final String doc = buildDocument(getSentences());                
+        report(doc);
+    }
+    
+} // End of the Class //
