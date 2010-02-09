c8b2d1595090829ad4fe3483cde66a7a5a13b30d
berlin.brown <berlin.brown@45ad1f16-f7af-11de-a831-516a40b15b2e>
2010-02-09 17:20:51 +0000

    adding pino windows

---
+++ trunk/ainotebook/pinobot/src/org/berlin/pino/win/PinoPanel.java
@@ -0,0 +1,18 @@
+package org.berlin.pino.win;
+
+import javax.swing.JPanel;
+
+import org.berlin.seesaw.swing.TeeterPanel;
+import org.berlin.seesaw.swing.layout.ITeeterLayout;
+
+public class PinoPanel extends TeeterPanel {
+
+    public PinoPanel(final JPanel panel, final ITeeterLayout layout) {
+        super(panel, layout);
+    }
+
+    public void constructView() {        
+        
+    }
+
+} // End of the Class //
