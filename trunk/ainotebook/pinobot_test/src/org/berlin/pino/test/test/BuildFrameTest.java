96c4bd6dcdfd00aff9cca3a672b16517882ac9e0
berlin.brown <berlin.brown@45ad1f16-f7af-11de-a831-516a40b15b2e>
2010-02-09 00:08:54 +0000

    adding frame test

--- trunk/ainotebook/pinobot_test/src/org/berlin/pino/test/test/BuildFrameTest.java
+++ trunk/ainotebook/pinobot_test/src/org/berlin/pino/test/test/BuildFrameTest.java
@@ -3,14 +3,26 @@ package org.berlin.pino.test.test;
 import junit.framework.TestCase;
 import junit.textui.TestRunner;
 
+import org.berlin.pino.win.BasicWindow;
 import org.berlin.seesaw.app.ITeeterWindow;
-import org.berlin.seesaw.app.TeeterWindow;
+import org.berlin.seesaw.swing.ITeeterButton;
+import org.berlin.seesaw.swing.ITeeterPanel;
+import org.berlin.seesaw.swing.TeeterButton;
+import org.berlin.seesaw.swing.TeeterPanel;
 
 public class BuildFrameTest extends TestCase {
 
     public void test1() {
         
-        final ITeeterWindow window = new TeeterWindow();
+        final ITeeterButton buttonEnter = new TeeterButton(null);
+        final ITeeterButton buttonClear = new TeeterButton(null);
+        final ITeeterButton buttonExit  = new TeeterButton(null);        
+        final ITeeterPanel panel = new TeeterPanel();
+        panel.addButton(buttonEnter);
+        panel.addButton(buttonClear);
+        panel.addButton(buttonExit);
+        
+        final ITeeterWindow window = (new BasicWindow.Builder()).withButtons(panel, buttonEnter, buttonClear, buttonExit).build();
     }
     
     public static void main(String [] args) {
