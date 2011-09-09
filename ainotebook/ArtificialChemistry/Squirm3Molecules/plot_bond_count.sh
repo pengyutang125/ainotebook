e273964901371b435c7298ce632a2192928de1f3
berlin.brown <berlin.brown@45ad1f16-f7af-11de-a831-516a40b15b2e>
2011-09-09 15:09:48 +0000

    Adding graphs for increasing bond count

---
+++ ainotebook/ArtificialChemistry/Squirm3Molecules/plot_bond_count.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Log plot on Y
+
+OUTPUT_IMG_FILE=plot_bond_count.png
+
+grep 'plotBondCount' 'logs/cell_simulation.log' > data_plotBondCount.dat
+
+echo "set terminal pngcairo size 1024,768
+set output '${OUTPUT_IMG_FILE}'
+set title 'Plot Bond Count By Iterations'
+set size 1,1
+set key left top
+set autoscale
+set xlabel 'Iteration'
+set ylabel 'TotalBonds'
+set style fill pattern
+set style histogram clustered
+set xtic rotate by -45 scale 0.8   
+set x2tics
+set grid
+set log y  
+plot  \\
+ 'data_plotBondCount.dat' u 2:3 t 'Bonds' w linespoints,  \\
+ '' u 2:4 t 'NoBonds' w linespoints
+" > gnuplot_tmp_cmd.tmp
+gnuplot gnuplot_tmp_cmd.tmp > /dev/null
---
+++ ainotebook/ArtificialChemistry/Squirm3Molecules/plot_reactions.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Log plot on Y
+
+OUTPUT_IMG_FILE=plot_reactions.png
+
+grep 'plotReactions' 'logs/cell_simulation.log' > data_reactions.dat
+
+echo "set terminal pngcairo size 1024,768
+set output '${OUTPUT_IMG_FILE}'
+set title 'Plot Reactions By Iterations'
+set size 1,1
+set key left top
+set autoscale
+set xlabel 'Iteration'
+set ylabel 'TotalReactionsByType'
+set style fill pattern
+set style histogram clustered
+set xtic rotate by -45 scale 0.8   
+set x2tics
+set grid
+set log y  
+plot  \\
+ 'data_reactions.dat' u 2:3 t 'R1:x3_y6->x2' w linespoints,  \\
+ '' u 2:4 t 'R2:x5/x0->x7' w linespoints
+" > gnuplot_tmp_cmd.tmp
+gnuplot gnuplot_tmp_cmd.tmp > /dev/null
